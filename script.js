b48a0da5d81f2cf1a04f9da965b501f1fbad6a54
Pradeep <jaiswalpradeep368@gmail.com>
2021-03-21 10:31:25 +0530

    refractor

---
+++ script.js
@@ -0,0 +1,29 @@
+var anchorTags = document.querySelectorAll('.navbar a');
+ 
+for(var i = 1; i < anchorTags.length; i++){
+
+        anchorTags[i].addEventListener("click", function(event){
+           event.preventDefault();
+        var targetId = this.textContent.toLowerCase();
+
+    
+        var targetSection = document.getElementById(targetId);
+                console.log(targetSection);
+                var interval = setInterval(function(){
+                var target  = targetSection.getBoundingClientRect();
+
+                if(target.top <= 0){
+                    clearInterval(interval);
+                    return;
+                }
+
+                window.scrollBy(0, 50);
+
+            }, 20);
+
+        })
+
+}
+
+var scrollbar = document.querySelectorAll('.skill-progress');
+
